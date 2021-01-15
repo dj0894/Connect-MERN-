e1691e4016b716caafc1d63d0730b8cfdd10fe90
Deepika Jha <deepikajha@Deepikas-MacBook-Pro.local>
2021-01-15 10:38:30 -0800

    updated depencies for front end.Added Client folder for React components.updated package.json to concurrently run React and mongodb.

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { Fragment, fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import './App.css';
+
+
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Landing />
+      </Fragment>
+    </Router>
+  );
+}
+
+export default App;
